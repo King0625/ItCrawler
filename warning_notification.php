e80bfdf03576b003d0f65f50d2693a575255087c
King0625 <a7164265@gmail.com>
2019-09-19 19:05:15 +0800

    Fix the channel error and add id to tag the slack member

--- warning_notification.php
+++ warning_notification.php
@@ -1,6 +1,9 @@
 <?php
 include('finished_day_crawler.php');
 include('config.php');
+include('member_id.php');
+
+header("Content-type: application/json");
 
 $camps = $status[0];
 $warning_members = array();
@@ -12,6 +15,7 @@ foreach($camps as $camp){
     }
 }
 
+// var_dump($warning_members);
 $backend_camp = ['shivaxsin', 'charleen_xu', 'd44041122', 'kenchen0625', 'louis222220', 'SarahCheng'];
 $android_camp = ['Kuroki', 'barnersh', 'larsnoya', 'zoeaeen13', 'jonec76'];
 $ios_camp = ['ytyubox', 'gg831006', 'emmatw', 'yuyuma17', 'Jes', 'henryluuu', 'aa08666', 'ablacktaco'];
@@ -21,20 +25,24 @@ $results = array();
 
 foreach($warning_members as $warning_member){
     if(in_array($warning_member, $web_camp)){
-        $results['web_camp'][] = $warning_member;
+        $results['web_camp'][] = $web_camp_id[$warning_member];
+        // $results['web_camp'][] = $warning_member;
     }elseif(in_array($warning_member, $android_camp)){
-        $results['android_camp'][] = $warning_member;
+        $results['android_camp'][] = $android_camp_id[$warning_member];
+        // $results['android_camp'][] = $warning_member;
     }elseif(in_array($warning_member, $backend_camp)){
-        $results['backend_camp'][] = $warning_member;
+        $results['backend_camp'][] = $backend_camp_id[$warning_member];
+        // $results['backend_camp'][] = $warning_member;
     }elseif(in_array($warning_member, $ios_camp)){
-        $results['ios_camp'][] = $warning_member;
+        $results['ios_camp'][] = $ios_camp_id[$warning_member];
+        // $results['ios_camp'][] = $warning_member;
     }
 }
 
-$web_string = " *web camp* : " . implode(', ', $results['web_camp']);
-$ios_string = " *ios camp* : " . implode(', ', $results['ios_camp']);
-$android_string = " *android camp* : " . implode(', ', $results['android_camp']);
-$backend_string = " *backend camp* : " . implode(', ', $results['backend_camp']);
+$web_string = " *web camp* : " . implode(' ', $results['web_camp']);
+$ios_string = " *ios camp* : " . implode(' ', $results['ios_camp']);
+$android_string = " *android camp* : " . implode(' ', $results['android_camp']);
+$backend_string = " *backend camp* : " . implode(' ', $results['backend_camp']);
 
 
 $webhook_url = WEB_HOOK_URL;
@@ -48,21 +56,21 @@ curl_setopt($ch, CURLOPT_POST, true);
 if(!$results == []){
     if(date("H:i") == date("H:i" , strtotime('21:30'))){
         $json_data = [
-            "text" => "@here :face_with_symbols_on_mouth: `21:30 未發文的同學` : \n " . $web_string . " \n " . $ios_string . " \n " . $android_string . " \n " . $backend_string
+            "text" => "<!here> :face_with_symbols_on_mouth: `21:30 未發文的同學` : \n " . $web_string . " \n " . $ios_string . " \n " . $android_string . " \n " . $backend_string
         ];
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($json_data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
         curl_exec($ch);
     
     }elseif(date("H:i") == date("H:i" , strtotime('21:00'))){
         $json_data = [
-            "text" => "@channel :rage: `21:00 未發文的同學` : \n " . $web_string . " \n " . $ios_string . " \n " . $android_string . " \n " . $backend_string
+            "text" => "<!channel> :rage: `21:00 未發文的同學` : \n " . $web_string . " \n " . $ios_string . " \n " . $android_string . " \n " . $backend_string
         ];
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($json_data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
         curl_exec($ch);
         
     }elseif(date("H:i") == date("H:i" , strtotime('18:00'))){
         $json_data = [
-            "text" => "@channel :thinking_face: `18:00 未發文的同學` : \n " . $web_string . " \n " . $ios_string . " \n " . $android_string . " \n " . $backend_string
+            "text" => "<!channel> :thinking_face: `18:00 未發文的同學` : \n " . $web_string . " \n " . $ios_string . " \n " . $android_string . " \n " . $backend_string
         ];
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($json_data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
         curl_exec($ch);
